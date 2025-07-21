62d3e617eab8878816ff6acb65bb38ed0e109acf
NyleLuza <Jluza21@gmail.com>
2025-07-21 16:29:01 -0700

    changed sizing and created three containers

--- jlc-web/src/App.js
+++ jlc-web/src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
+import Navbar from './components/Navbar'
 import './App.css';
 
 function App() {
   return (
-    <div className="d-flex flex-column">
-      <nav className="nav">
-        <h1>
-          Jeremias Luza Construction
-        </h1>
-      </nav>
+    <div className="d-flex flex-column justify-content-center h-100">
+      <Navbar />
+      <p className="d-flex flex-grow-1 min-vh-100">
+          <p>Loremdf</p>
+      </p>
+      <footer className="d-flex flex-grow-1">
+        This is the footer
+      </footer>
     </div>
   );
 }
