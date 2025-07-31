a7b723eb489b3067ed299964a167a93ce63bc4ed
NyleLuza <Jluza21@gmail.com>
2025-07-30 21:26:11 -0700

    added contact info onto the footer

---
+++ jlc-web/src/components/Footer.js
@@ -0,0 +1,13 @@
+function Footer(){
+    return(
+        <footer className="d-flex flex-grow-1" style={{minHeight: "350px", backgroundColor: "#40509d"}}>
+            <div className="d-flex flex-column justify-content-center flex-grow-1" style={{paddingLeft: "120px",paddingRight: "80px",fontFamily: 'Helvetica', color: '#F5F5F5'}}>
+                <h1 className="d-flex"style={{paddingBottom: "20px"}}>Contact Us</h1>
+                <h4>Tel: 818-234-2124</h4>
+                <h4>Email: jbl@const.com</h4>
+            </div>
+            <div className="d-flex flex-grow-1">df</div>
+      </footer>
+    )
+}
+export default Footer;
