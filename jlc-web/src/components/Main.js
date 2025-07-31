16c292e0425fa1f3bf7408e01baa1de29e09956f
NyleLuza <Jluza21@gmail.com>
2025-07-30 20:50:10 -0700

    added components for main and footer

---
+++ jlc-web/src/components/Main.js
@@ -0,0 +1,10 @@
+import img from './imgs/house1.jpg'
+
+function Main(){
+    return(
+        <div className="d-flex flex-grow-1">
+          <img src={img} alt="Example" style={{width: "100%", height: "100%", objectFit: "cover"}}/>
+      </div>
+    )
+}
+export default Main;
