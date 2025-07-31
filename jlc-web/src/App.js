ea58829a14df54bc542e32a6874cb8957d207975
NyleLuza <Jluza21@gmail.com>
2025-07-30 20:50:10 -0700

    added components for main and footer

--- jlc-web/src/App.js
+++ jlc-web/src/App.js
@@ -1,14 +1,14 @@
 import Navbar from './components/Navbar'
 import img from './imgs/house1.jpg'
 import './App.css';
+import Footer from './components/Footer';
+import Main from './components/Main'
 
 function App() {
   return (
     <div className="d-flex flex-grow-1 flex-column h-100">
       <Navbar />
-      <div className="d-flex flex-grow-1">
-          <img src={img} alt="Example" style={{width: "100%", height: "100%", objectFit: "cover"}}/>
-      </div>
+      <Main />
       <footer className="d-flex flex-grow-1">
         This is the footer
       </footer>
