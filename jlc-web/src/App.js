533a457bd12368b9559d3d010e5624e7886393ec
NyleLuza <Jluza21@gmail.com>
2025-07-30 20:32:53 -0700

    adjusted the imported image so that it fits container and added a notes txt to keep note on challenges

--- jlc-web/src/App.js
+++ jlc-web/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="d-flex flex-grow-1 flex-column h-100">
       <Navbar />
       <div className="d-flex flex-grow-1">
-          <img src={img} alt="Example"/>
+          <img src={img} alt="Example" style={{width: "100%", height: "100%", objectFit: "cover"}}/>
       </div>
       <footer className="d-flex flex-grow-1">
         This is the footer
