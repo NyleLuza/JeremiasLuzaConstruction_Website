fe49a41c551633c60385dd2273f033710109ef8e
NyleLuza <Jluza21@gmail.com>
2025-08-05 15:38:55 -0700

    reshaping main

--- jlc-web/client/src/components/Main.js
+++ jlc-web/client/src/components/Main.js
@@ -1,14 +1,37 @@
-import img from "./imgs/house1.jpg";
+import img from "./imgs/house1_front.jpg";
 
 function Main() {
   return (
-    <div className="d-flex flex-grow-1" style={{ minheight: "100vh" }}>
-      <img
+    <div
+      className="d-flex flex-column flex-grow-1"
+      style={{ minheight: "100vh", paddingTop: "60px" }}
+    >
+      {/*<img
         src={img}
         alt="Example"
         style={{ width: "100%", height: "100%", objectFit: "cover" }}
-      />
-      {/*<div className="d-flex " style={{ height: "3000px" }}></div>*/}
+      />*/}
+
+      {/* first section of the body */}
+      <div
+        className="d-flex flex-grow-1"
+        style={{ height: "500px", color: "black" }}
+      >
+        <div className="d-flex" style={{ height: "500px", color: "black" }}>
+          <img
+            src={img}
+            alt="Example"
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+          />
+        </div>
+        <div className="d-flex flex-grow-1">sdfsf</div>
+      </div>
+      <div
+        className="d-flex flex-grow-1 "
+        style={{ height: "500px", color: "black" }}
+      >
+        heasdfasfdad
+      </div>
     </div>
   );
 }
