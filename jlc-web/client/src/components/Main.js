1bb7429979ab33d9c35832f3584ea97c25a7caa5
NyleLuza <Jluza21@gmail.com>
2025-08-07 12:53:53 -0700

    added image as a background image property and added notes about it

--- jlc-web/client/src/components/Main.js
+++ jlc-web/client/src/components/Main.js
@@ -6,25 +6,45 @@ function Main() {
       className="d-flex flex-column flex-grow-1"
       style={{ minheight: "100vh", paddingTop: "60px" }}
     >
+      {/*
       <img
         src={img}
         alt="Example"
         style={{ width: "100%", height: "100%", objectFit: "cover" }}
       />
-      {/*
-      {/* first section of the body
+      */}
+      {/* first section of the body*/}
       <div
         className="d-flex flex-grow-1"
         style={{ height: "500px", color: "black" }}
       >
-        <div className="d-flex" style={{ height: "500px", color: "black" }}>
+        <div
+          className="d-flex flex-grow-1"
+          style={{
+            backgroundImage: `URL(${img})`,
+            backgroundSize: "cover",
+            height: "calc(100vh - 60px)",
+            color: "black",
+          }}
+        >
+          {/*
           <img
+            className="d-flex"
             src={img}
             alt="Example"
-            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              position: "relative",
+            }}
           />
+          
+          <div className="d-flex" style={{ position: "absolute" }}>
+            sdfsdss
+          </div>
+          */}
         </div>
-        <div className="d-flex flex-grow-1">sdfsf</div>
       </div>
       <div
         className="d-flex flex-grow-1 "
@@ -32,7 +52,6 @@ function Main() {
       >
         heasdfasfdad
       </div>
-      */}
     </div>
   );
 }
