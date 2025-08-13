a6edd6f879056b6c7a97efe6377aafbc21ef6353
NyleLuza <Jluza21@gmail.com>
2025-08-13 14:44:23 -0700

    deleted extra src folder

--- jlc-web/client/src/App.js
+++ jlc-web/client/src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/Projects" element={<Projects />} />
       </Routes>
--- jlc-web/src/components/button/Button.js
+++
@@ -1,6 +0,0 @@
-function Button({label, onClick}){
-    return(
-        <button className="btn btn-primary btn-sm text-uppercase" style={{backgroundColor:'#40509d', fontFamily: 'Helvetica', color: '#F5F5F5',fontSize: '16px', border: 'none'}}>{label}</button>
-    );
-}
-export default Button;
