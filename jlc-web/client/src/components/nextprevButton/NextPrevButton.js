1129451447818fada5e4d9d56006dae89b5af045
NyleLuza <Jluza21@gmail.com>
2025-08-18 16:47:35 -0700

    added transition to text on body

--- jlc-web/client/src/components/nextprevButton/NextPrevButton.js
+++ jlc-web/client/src/components/nextprevButton/NextPrevButton.js
@@ -5,6 +5,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 function NextPrevButton() {
+  const [isActive, setIsActive] = useState(true);
   const [buttonState, setButtonState] = useState({
     prevRemodelType: "remodel",
     prevAddress: "address",
@@ -26,6 +27,10 @@ function NextPrevButton() {
       console.error(errorMessage);
       alert(errorMessage);
     }
+    setIsActive(false);
+    setTimeout(() => {
+      setIsActive(true); // fade back in
+    }, 300); // matches CSS duration
     setButtonState({
       prevRemodelType: houses.remodelType,
       prevAddress: houses.address,
@@ -49,13 +54,9 @@ function NextPrevButton() {
             backgroundImage: `URL(${buttonState.prevImage})`,
             backgroundSize: "cover",
             backgroundPosition: "center center",
+            transition: "0.8s",
           }}
-        >
-          <img
-            src={buttonState.prevImage}
-            style={{ width: "100%", height: "100%" }}
-          />
-        </div>
+        ></div>
       </div>
 
       {/*right hand side for text and description*/}
@@ -71,11 +72,20 @@ function NextPrevButton() {
         </div>
         <div
           className="d-flex align-items-center flex-column"
-          style={{ height: "100%" }}
+          style={{
+            height: "100%",
+            opacity: isActive ? 1 : 0,
+            transform: isActive ? "translateX(0)" : "translateX(-30px)",
+            transition: "all 0.5s ease",
+          }}
         >
           <h2>{buttonState.prevRemodelType}</h2>
-          <div>{buttonState.prevAddress}</div>
-          <div>{buttonState.prevDescription}</div>
+          <div style={{ transition: "#000000 0.3s ease-in-out" }}>
+            {buttonState.prevAddress}
+          </div>
+          <div style={{ transition: "#000000 4s ease-in-out" }}>
+            {buttonState.prevDescription}
+          </div>
         </div>
         <div className="d-flex justify-content-end">
           <button onClick={handleNextClick}>Prev</button>
